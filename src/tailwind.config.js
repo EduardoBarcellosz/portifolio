a37d27b1556f1ab31e4f0d5a37f580795ead7572
Eduardo <barcelloser@gmail.com>
2024-10-14 03:59:19 -0300

    tailwind att

--- src/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/**/*.{html,js,ts,jsx,tsx}", // Inclua todos os arquivos HTML, JS, TS, JSX, e TSX dentro da pasta src
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
-
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Inclua todos os arquivos HTML, JS, TS, JSX, e TSX dentro da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
