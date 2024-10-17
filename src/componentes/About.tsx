0357c2eb7e7ad263d8a0e27afcf89adb42a43dce
Eduardo <barcelloser@gmail.com>
2024-10-17 15:52:39 -0300

    about add

--- src/componentes/About.tsx
+++ src/componentes/About.tsx
@@ -5,6 +5,14 @@ const About: React.FC = () => {
     return (
         <section id="about" className="about-section">
         <h3>Sobre</h3>
+        <p>Olá, meu nome é Eduardo Barcellos, 
+            sou estudante de Engenharia de Software e tenho uma paixão por aprender tecnologias. 
+            Sempre busco me aprofundar em novas ferramentas e linguagens, 
+            especialmente aquelas que me ajudam a construir soluções inovadoras. 
+            Tenho interesse em desenvolvimento web, banco de dados, 
+            e também adoro desafios que envolvem otimização e design de sistemas. 
+            Além disso, estou constantemente explorando o universo da programação 
+            para expandir meus conhecimentos e aplicá-los em projetos práticos.</p>
         </section>
     );
 }
