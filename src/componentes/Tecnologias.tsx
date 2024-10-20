f8af10bca330be7ba67252dcd94459fe7e5b6913
Eduardo <barcelloser@gmail.com>
2024-10-20 17:07:28 -0300

    tecnologias update

--- src/componentes/Tecnologias.tsx
+++ src/componentes/Tecnologias.tsx
@@ -1,13 +1,102 @@
-
 export const Tecnologias = () => {
-    return( 
-        <section id="tecnologias" className="tecnologias-section">
-            <h3 style={{color: '#9669FF'}}>Tecnologias</h3>
-
-
-
-        </section>
-    );
-}
+  return (
+    <section id="tecnologias" className="tecnologias-section">
+      <h3 style={{ color: "#9669FF" }}>Tecnologias</h3>
+      <div className="tecnologias-display">
+        <div className="tecnologia" id="html">
+          <img
+            src="https://img.icons8.com/color/48/000000/html-5--v1.png"
+            alt="html"
+          />
+          <p>HTML5</p>
+        </div>
+        <div className="tecnologia" id="css">
+          <img
+            src="https://img.icons8.com/color/48/000000/css3.png"
+            alt="css"
+          />
+          <p>CSS3</p>
+        </div>
+        <div className="tecnologia" id="javascript">
+          <img
+            src="https://img.icons8.com/color/48/000000/javascript--v1.png"
+            alt="javascript"
+          />
+          <p>JavaScript</p>
+        </div>
+        <div className="tecnologia" id="typescript">
+          <img
+            src="https://img.icons8.com/color/48/000000/typescript.png"
+            alt="typescript"
+          />
+          <p>TypeScript</p>
+        </div>
+        <div className="tecnologia" id="react">
+          <img
+            src="https://img.icons8.com/color/48/000000/react-native.png"
+            alt="react"
+          />
+          <p>React</p>
+        </div>
+        <div className="tecnologia" id="node">
+          <img
+            src="https://img.icons8.com/color/48/000000/nodejs.png"
+            alt="node"
+          />
+          <p>Node.js</p>
+        </div>
+        <div className="tecnologia" id="mongodb">
+          <img
+            src="https://img.icons8.com/color/48/000000/mongodb.png"
+            alt="mongodb"
+          />
+          <p>MongoDB</p>
+        </div>
+        <div className="tecnologia" id="python">
+          <img
+            src="https://img.icons8.com/color/48/000000/python.png"
+            alt="python"
+          />
+          <p>Python</p>
+        </div>
+        <div className="tecnologia" id="java">
+          <img
+            src="https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png"
+            alt="java"
+          />
+          <p>Java</p>
+        </div>
+        <div className="tecnologia" id="c">
+          <img
+            src="https://img.icons8.com/color/48/000000/c-programming.png"
+            alt="c"
+          />
+          <p>C</p>
+        </div>
+        <div className="tecnologia" id="docker">
+          <img
+            src="https://img.icons8.com/color/48/000000/docker.png"
+            alt="docker"
+          />
+          <p>Docker</p>
+        </div>
+        <div className="tecnologia" id="springboot">
+          <img
+            src="https://img.icons8.com/color/48/000000/spring-logo.png"
+            alt="spring boot"
+          />
+          <p>Spring Boot</p>
+        </div>
+        <div className="tecnologia" id="figma">
+          <img
+            src="https://img.icons8.com/color/48/000000/figma.png"
+            alt="figma"
+          />
+          <p>Figma</p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Tecnologias;
